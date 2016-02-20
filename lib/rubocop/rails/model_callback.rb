74feb59e74cefdc1b8b2cbc614f802283858a15c
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-02-20 16:44:44 +0900

    Initialise commit

---
+++ lib/rubocop/rails/model_callback.rb
@@ -0,0 +1,9 @@
+require "rubocop/rails/model_callback/version"
+
+module Rubocop
+  module Rails
+    module ModelCallback
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubocop/rails/model_callback/version.rb
@@ -0,0 +1,7 @@
+module Rubocop
+  module Rails
+    module ModelCallback
+      VERSION = "0.1.0"
+    end
+  end
+end
