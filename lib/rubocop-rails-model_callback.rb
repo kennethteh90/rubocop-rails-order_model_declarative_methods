a0fcdca1d8e7c12e1fb9c4ce5adb97fbd4dbe0d4
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-02-20 17:23:40 +0900

    default.yml

--- lib/rubocop-rails-model_callback.rb
+++ lib/rubocop-rails-model_callback.rb
@@ -1,3 +1,8 @@
 require 'rubocop'
 require 'rubocop/rails/model_callback/version'
+module RuboCop::RSpec; end
+require 'rubocop/rails/inject'
+
+RuboCop::Rails::Inject.defaults!
+
 require 'rubocop/cop/rails/model_callback'
--- lib/rubocop/cop/rails/model_callback.rb
+++ lib/rubocop/cop/rails/model_callback.rb
@@ -1,2 +1,12 @@
-class RuboCop::Cop::Rails::ModelCallback
+require 'pry'
+
+module RuboCop
+  module Cop
+    module Rails
+      class ModelCallback < Cop
+        def on_class(node)
+        end
+      end
+    end
+  end
 end
