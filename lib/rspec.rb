7d456cdf1926c974c378c977cf5607efe0ddf736
Kenneth Teh <kennethteh90@gmail.com>
2020-08-31 22:40:52 +0800

    Fix CONFIG_DEFAULT

---
+++ lib/rspec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+# This file is copied from rubocop-rspec
+
+module RuboCop
+  # RuboCop RSpec project namespace
+  module RSpec
+    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
+    CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
+    CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read).freeze
+
+    private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
+  end
+end
--- lib/rubocop/rspec/inject.rb
+++ lib/rubocop/rspec/inject.rb
@@ -3,12 +3,6 @@
 
 module RuboCop
   module RSpec
-    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.parent.expand_path.freeze
-    CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
-    CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read).freeze
-
-    private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
-
     # Because RuboCop doesn't yet support plugins, we have to monkey patch in a
     # bit of our configuration.
     module Inject
